43df1b0560642aab24cb3c6bbc8cfc5b7c472b48
niclausslumoring <niclausslumoring97@gmail.com>
2023-04-12 14:43:10 +0700

    Updated 12 Apr 23

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -19,6 +19,10 @@ import SimpleCurencyConverter from "../../assets/currencyconverter.png";
 import Shooting from "../../assets/shooting.png";
 import GameOver from "../../assets/gameovercorona.png";
 import Sanitizer from "../../assets/sanitizer.png";
+import DataScience0 from "../../assets/365-Data-Science.png";
+import DataScience1 from "../../assets/datascienceP1.png";
+import DataScience2 from "../../assets/datascienceP2.png";
+import DataScience3 from "../../assets/datascienceP3.png";
 
 export const projectsData = [
   {
@@ -137,6 +141,29 @@ export const projectsData = [
     language: "Javascript using Phaser Framework",
     link: "https://corona-buster-byniclauss.netlify.app/",
     link_placeholder: "Game Link",
+  },
+  {
+    id: 10,
+    image: DataScience0,
+    title: "Descriptive Statistic",
+    category: "data science",
+    detail_job:
+      "I enrolled in an online course called 'The Data Science Course 2023: Complete Data Science Bootcamp' on Udemy. During the course, I focused on learning about statistics in detail. One of the practical exercises I completed was related to descriptive statistics. In this exercise, I was given a dataset to work with and had to analyze it based on specific requirements provided by the course instructors.",
+    date_job: "April 2023",
+    image_detail: DataScience1,
+    image_detail2: DataScience2,
+    language: "Excel",
+  },
+  {
+    id: 11,
+    image: DataScience0,
+    title: "Confidence Interval & Z-value",
+    category: "data science",
+    detail_job:
+      "I took an online course on Udemy called 'The Data Science Course 2023: Complete Data Science Bootcamp'. The course focused on teaching statistics in great detail, and I participated in a practical exercise that covered Confidence Interval, Population Variance, and Z-value. For the exercise, the course instructors provided a dataset that I had to analyze based on their specific requirements.",
+    date_job: "April 2023",
+    image_detail: DataScience3,
+    language: "Excel",
   },
 ];
 
