f85be36acf858606f6d4f68de3b5c03bbeaf3817
niclausslumoring <niclausslumoring97@gmail.com>
2023-05-25 12:44:06 +0700

    tambahan 25 Mei

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -23,6 +23,10 @@ import DataScience0 from "../../assets/365-Data-Science.png";
 import DataScience1 from "../../assets/datascienceP1.png";
 import DataScience2 from "../../assets/datascienceP2.png";
 import DataScience3 from "../../assets/datascienceP3.png";
+import DataScience4 from "../../assets/datascienceP4.png";
+import DataScience5 from "../../assets/datascienceP5.png";
+import BookStore from "../../assets/bookstore.png";
+import Work15 from "../../assets/work14.png";
 
 export const projectsData = [
   {
@@ -157,13 +161,36 @@ export const projectsData = [
   {
     id: 11,
     image: DataScience0,
-    title: "Confidence Interval & Z-value",
+    title: "Z-value",
     category: "data science",
     detail_job:
       "I took an online course on Udemy called 'The Data Science Course 2023: Complete Data Science Bootcamp'. The course focused on teaching statistics in great detail, and I participated in a practical exercise that covered Confidence Interval, Population Variance, and Z-value. For the exercise, the course instructors provided a dataset that I had to analyze based on their specific requirements.",
     date_job: "April 2023",
     image_detail: DataScience3,
     language: "Excel",
+  },
+  {
+    id: 12,
+    image: DataScience0,
+    title: "Confidence Interval",
+    category: "data science",
+    detail_job:
+      "I took an online course on Udemy called 'The Data Science Course 2023: Complete Data Science Bootcamp'. The course focused on teaching statistics in great detail, and I participated in a practical exercise that covered Confidence Interval, Population Variance, and Z-value. For the exercise, the course instructors provided a dataset that I had to analyze based on their specific requirements.",
+    date_job: "April 2023",
+    image_detail: DataScience4,
+    image_detail2: DataScience5,
+    language: "Excel",
+  },
+  {
+    id: 13,
+    image: BookStore,
+    title: "Jramedia",
+    category: "web design",
+    detail_job:
+      "I build a website with PHP and Laravel that serves as the final destination for buying books and office supplies. This website can browse a broad selection of books and office supplies, a sizable product catalog, and a safe checkout procedure. Take advantage of convenient order tracking, easy search options, and personalized recommendations",
+    date_job: "Mei 2023",
+    image_detail: Work15,
+    language: "Laravel",
   },
 ];
 
